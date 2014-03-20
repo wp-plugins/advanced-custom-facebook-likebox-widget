f4af52b4d32eb70ceda9ee1db66ae547da575fae
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-20 05:52:48 +0000

    adding intial version
    git-svn-id: https://plugins.svn.wordpress.org/advanced-custom-facebook-likebox-widget/trunk@878553 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ advancedCustomFacebookLikeboxWidget.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * @package advanced-custom-facebook-likebox-widget
+*/
+/*
+Plugin Name: Advanced Custom Facebook Likebox Widget
+Plugin URI: http://www.sparxseo.com
+Description: Advanced Custom Facebook Likebox Widget is a customizable facebook likebox wordpress widget. There have lots of options to customize the likebox widget. So by using this you can up and run a customizable wordpress widget on your website very easily.
+Version: 1.0
+Author: Alan Ferdinand
+Author URI: http://www.sparxseo.com
+*/
+
+class advancedCustomFacebookLikeboxWidget extends WP_Widget{
+    public function __construct() {
+        $params = array(
+            'description' => 'Advanced Custom Facebook Likebox Widget is a customizable facebook likebox wordpress widget. There have lots of options to customize the likebox widget. So by using this widget you can up and run a customizable wordpress widget on your website very easily',
+            'name' => 'Advanced Custom Facebook Likebox Widget'
+        );
+        parent::__construct('advancedCustomFacebookLikeboxWidget','',$params);
+    }
+    public function form($instance) {
+        extract($instance);
+        ?>
+<script type="text/javascript">
+//<![CDATA[
+    jQuery(document).ready(function()
+    {
+	// colorpicker field
+	jQuery('.cw-color-picker').each(function(){
+	var $this = jQuery(this),
+        id = $this.attr('rel');
+ 	$this.farbtastic('#' + id);
+    });
+});		
+//]]>   
+</script>
+<!-- Advanced Custom Facebook Likebox Widget Configuration Fields START - -->
+<div style="color:#fff; font-size:12px; font-weight:bold; padding:3px; margin:0; text-align:center; background:#333333;">Basic Configuration</div>
+<p>
+    <label for="<?php echo $this->get_field_id('title');?>">Title : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('title');?>"
+	name="<?php echo $this->get_field_name('title');?>"
+        value="<?php echo !empty($title) ? $title : "Advanced Custom Facebook Likebox Widget"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('suffix');?>">Module Class Suffix : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('suffix');?>"
+	name="<?php echo $this->get_field_name('suffix');?>"
+    value="<?php echo !empty($suffix) ? $suffix : ""; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('page');?>">Facebook Page URL : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('page');?>"
+	name="<?php echo $this->get_field_name('page');?>"
+    value="<?php echo !empty($page) ? $page : "http://www.facebook.com/FacebookDevelopers"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('width');?>">Width : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('width');?>"
+	name="<?php echo $this->get_field_name('width');?>"
+    value="<?php echo !empty($width) ? $width : "300"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('height');?>">Height : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('height');?>"
+	name="<?php echo $this->get_field_name('height');?>"
+    value="<?php echo !empty($height) ? $height : "356"; ?>" />
+</p>
+<div style="color:#fff; font-size:12px; font-weight:bold; padding:3px; margin:0; text-align:center; background:#333333;">Advanced Configuration</div>
+<p>
+    <label for="<?php echo $this->get_field_id('background'); ?>">Background Color :</label> 
+    <input
+    	class="widefat background"
+   	id="<?php echo $this->get_field_id('background'); ?>"
+        name="<?php echo $this->get_field_name('background'); ?>"
+        value="<?php if($background) { echo $background; } else { echo '#050026';} ?>" />
+</p>
+<div class="cw-color-picker backgroundHide" rel="<?php echo $this->get_field_id('background'); ?>"></div>
+<p>
+    <label for="<?php echo $this->get_field_id( 'header' ); ?>">Show Header:</label> 
+    <select id="<?php echo $this->get_field_id( 'header' ); ?>"
+        name="<?php echo $this->get_field_name( 'header' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($header == 'true') echo 'selected="true"'; ?> > Yes</option>
+            <option value="false" <?php if ($header == 'false') echo 'selected="false"'; ?> >No</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('padding');?>">Padding : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('padding');?>"
+	name="<?php echo $this->get_field_name('padding');?>"
+        value="<?php echo !empty($padding) ? $padding : "15"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('radius');?>">Radius Size : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('radius');?>"
+	name="<?php echo $this->get_field_name('radius');?>"
+        value="<?php echo !empty($radius) ? $radius : "15"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('border');?>">Border Size : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('border');?>"
+	name="<?php echo $this->get_field_name('border');?>"
+        value="<?php echo !empty($border) ? $border : "2"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('bordercolor'); ?>">Border Color :</label> 
+    <input
+    	class="widefat borderColor"
+   	id="<?php echo $this->get_field_id('bordercolor'); ?>"
+        name="<?php echo $this->get_field_name('bordercolor'); ?>"
+        value="<?php if($bordercolor) { echo $bordercolor; } else { echo '#9A1717';} ?>" />
+</p>
+<div class="cw-color-picker bordercolorHide" rel="<?php echo $this->get_field_id('bordercolor'); ?>"></div>
+<p>
+    <label for="<?php echo $this->get_field_id( 'theme' ); ?>">Color Scheme :</label> 
+    <select id="<?php echo $this->get_field_id( 'theme' ); ?>"
+        name="<?php echo $this->get_field_name( 'theme' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="light" <?php if ($theme == 'light') echo 'selected="light"'; ?> >Light</option>
+            <option value="dark" <?php if ($theme == 'dark') echo 'selected="dark"'; ?> >Dark</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'author' ); ?>">Promote Author:</label> 
+    <select id="<?php echo $this->get_field_id( 'author' ); ?>"
+        name="<?php echo $this->get_field_name( 'author' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($author == 'true') echo 'selected="true"'; ?> >Ok Sure!</option>
+            <option value="false" <?php if ($author == 'false') echo 'selected="false"'; ?> >No</option>	
+    </select>
+</p>
+<!--end of configuration fields-->
+<?php
+    }
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+        $data = "";
+        
+        $data .= "
+            <script>(function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = '//connect.facebook.net/en_US/all.js#xfbml=1';
+                fjs.parentNode.insertBefore(js, fjs);
+              }(document, 'script', 'facebook-jssdk'));
+             </script>
+  ";
+        $data .= "
+<div id='advanced_facebook_likebox_widget' class='$suffix' style='max-width: $width";
+        $data .= "px;'>
+		<div class='fbCustom' style='background:$background; width:$width";
+        $data .= "px; padding: $padding";
+        $data .= "px; border: $border";
+        $data .= "px solid $bordercolor; border-radius: $radius";
+        $data .= "px;'>
+        <div class='FBWrap' style='height:$height";
+        $data .= "px; overflow: hidden;'>
+            <div class='fb-like-box'
+                data-width='$width' data-height='$height' 
+                data-href='$page'
+                data-border-color='$bordercolor' data-show-faces='true'
+                data-stream='false' data-header='false' data-color-scheme='dark'>
+            </div>
+         </div>
+      </div>
+</div>            
+";
+if($author == "true"){
+	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.sparxseo.com' target='_blank' style='color: #808080;' title='Powered by SparxSEO'>SparxSEO.COM</a></div>";}
+            echo $before_widget;
+            echo $before_title . $title . $after_title;
+            echo $data;
+            echo $after_widget;
+    }
+}
+//color picker module codies
+function sparxfb_sample_load_color_picker_script() {
+	wp_enqueue_script('farbtastic');
+}
+function sparxfb_sample_load_color_picker_style() {
+	wp_enqueue_style('farbtastic');
+}
+add_action('admin_print_scripts-widgets.php', 'sparxfb_sample_load_color_picker_script');
+add_action('admin_print_styles-widgets.php', 'sparxfb_sample_load_color_picker_style');
+//start registering the extension
+add_action('widgets_init','register_sparx_advancedCustomFacebookLikeboxWidget');
+function register_sparx_advancedCustomFacebookLikeboxWidget(){
+    register_widget('advancedCustomFacebookLikeboxWidget');
+}
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Advanced Custom Facebook Likebox Widget ===
+
+Plugin URI: http://www.sparxseo.com
+Author Name : Alan Ferdinand
+Author URL : http://www.sparxseo.com
+Tags: advanced custom facebook likebox widget, wordpress widget, advanced facebook, custom facebook, facebook likebox, facebook widget, facebook for wordpress, facebook widget for wordpress, super customizable facebook likebox
+Requires at least: 3.0.1
+Tested up to: 3.8
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Advanced Custom Facebook Likebox Widget is free facebook likebox display widget. Using this widget you can show facebook connections on your wordpress website.
+
+== Description ==
+
+Thanks for installing our developed wordpress widget.
+Everybody enjoys corresponding with friends, sharing photos, and posting comments. Technology has truly played a momentous role in today’s age, and it has spread to all nations. Technology has evolved significantly over time, once very simplistic, and now, highly innovative and complex. In previous years, technology was a precious commodity intended for the elite and wealthy. Now, it can be accessed by individuals of all socioeconomic classes. 
+Facebook has disseminated rapidly throughout the world, and many Facebook users are highly addicted to this interface. 
+Facebook is widely acclaimed as one of the best online, social networking interfaces to ever grace the web. Facebook has harnessed an unparalleled level of popularity. 
+Using this module is very easy.
+
+Features of Advanced Custom Facebook Likebox Widget
+
+	* Can set a custom background on your facebook likebox.
+	* Many customizable options you can ever think can possible with facebook likebox before.
+	* Highly Customizable - as well can easily make tons of changes from backend.
+	* Work with most of recent wordpress released versions.
+	* Responsive - it make sure you able to enjoy our widget on mobile devices as well.
+	* Two color schemes available.
+
+== Installation ==
+
+Check the documentation folder. or go here - http://sparxseo.com/documentation/advanced-custom-facebook-likebox-widget.html
+
+== Frequently Asked Questions ==
+ Contact us for any kind of support - admin@sparxseo.com
+
+== Screenshots ==
+
+1. Frontend Screenshot
+2. Backend Screenshot
+
+== Changelog ==
+
+= 1.0 =
+Stable version release - 19th March 2014
