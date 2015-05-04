32c923aa8452db0c3427df1322628b0d26b248fa
alan.developer <alan.developer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 07:29:11 +0000

    some changes done
    git-svn-id: https://plugins.svn.wordpress.org/advanced-custom-facebook-likebox-widget/trunk@1152648 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- advancedCustomFacebookLikeboxWidget.php
+++ advancedCustomFacebookLikeboxWidget.php
@@ -209,7 +209,7 @@ class advancedCustomFacebookLikeboxWidget extends WP_Widget{
             </div>
          </div>";
 if($author != "false"){
-	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.tensunitdepot.com/' target='_blank' style='color: #808080;' title='visit the homepage'>tens units</a></div>";}
+	$data .= "<div style='font-size: 9px; color: #808080; font-weight: normal; font-family: tahoma,verdana,arial,sans-serif; line-height: 1.28; text-align: right; direction: ltr;'><a href='http://www.tensunitdepot.com/' target='_blank' style='color: #808080;' title='visit the website'>click here</a></div>";}
 $data .= "</div></div>";           
 
             echo $before_widget;
